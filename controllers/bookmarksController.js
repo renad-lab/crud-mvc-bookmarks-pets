143d6c8f6f247951829bc5a8888985835991ddcc
renad-lab <renad@pursuit.org>
2024-06-24 11:31:36 -0400

    update codes

--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -1,17 +1,26 @@
-const express = require('express')
+const express = require("express");
 // Create an instance of our express Router that our server can use to route appropriately
-const bookmarks = express.Router()
+const bookmarks = express.Router();
 
 // Import the bookmarks model
-const bookmarksArray = require('../models/bookmark')
-
+const bookmarksArray = require("../models/bookmark");
 
 // Index Routes: gets all of the bookmarks
 // localhost:4001/bookmarks/
-bookmarks.get('/', (req, res) => {
-    res.json(bookmarksArray)
-})
+bookmarks.get("/", (req, res) => {
+  res.json(bookmarksArray);
+});
 
+//SHOW Route: gets ONE of the bookmarks
+bookmarks.get("/:arrayIndex", (req, res) => {
+  const { arrayIndex } = req.params;
+  if (bookmarksArray[arrayIndex]) {
+    res.json(bookmarksArray[arrayIndex]);
+  } else {
+    res.json({ error: "Not Found" });
+  }
+});
 
+// CREATE Route: creates a new bookmark
 
-module.exports = bookmarks
+module.exports = bookmarks;
--- controllers/petsController.js
+++ controllers/petsController.js
@@ -1,12 +1,19 @@
-const express = require('express')
-const pets = express.Router()
-const petsArray = require('../models/pet')
-
+const express = require("express");
+const pets = express.Router();
+const petsArray = require("../models/pet");
 
 // localhost:4001/pets/
-pets.get('/', (req, res) => {
-    res.json(petsArray)
-})
+pets.get("/", (req, res) => {
+  res.json(petsArray);
+});
 
+pets.get("/:arrayIndex", (req, res) => {
+  const { arrayIndex } = req.params;
+  if (petsArray[arrayIndex]) {
+    res.json(petsArray[arrayIndex]);
+  } else {
+    res.json({ error: "Not Found" });
+  }
+});
 
-module.exports = pets
+module.exports = pets;
